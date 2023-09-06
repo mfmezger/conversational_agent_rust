a5db951691d8a3d47af602f3279c542e0457ca19
Marc Fabian Mezger <marc.mezger@gmail.com>
2023-09-06 20:26:42 +0200

    wip.

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,15 @@ async fn index() -> impl Responder {
     "Hello, Actix-Web!"
 }
 
+async fn qa() -> impl Responder {
+    "Hello, QA!"
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
-        App::new().route("/", web::get().to(index))
+        App::new().route("/", web::get().to(index)).route("/qa", web::get().to(qa))
+    
     })
     .bind("127.0.0.1:8080")?
     .run()
